d225b29243a7d6df045e2fc1a33e4e019cdcd485
MaximStrelinicov <strelinicov@inbox.ru>
2023-10-05 18:48:00 +0300

    Cerintiile lab 1

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("helle world");
+
+        System.out.printf("Hello and welcome!");
+
+        System.out.println(2*5);
+        System.out.println(8/2);
+        System.out.println(3+3);
+        System.out.println(35+5);
+
+        System.out.println(128-28);
+
+
+        int myNum;
+        myNum = 15;
+        System.out.println("myNum");
+        System.out.println(myNum);
+
+        String fristName = "Maxim";
+        String lastName = "Strelinicov";
+        String fullNume = fristName + lastName;
+
+        System.out.println(fullNume);
+
+
+        int a = 5, x = 6 , y = 5;
+
+        System.out.println(a+x+y);
+
+        int c, v, b ;
+        c = v = b = 50;
+
+        System.out.println(c+v+b);
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("i = " + i);
+        }
+    }
+}
