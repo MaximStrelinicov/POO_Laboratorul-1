e68463ba1a541a13dc53f142fdc39c1e5398d02a
MaximStrelinicov <strelinicov@inbox.ru>
2023-10-07 11:23:07 +0300

    Eliminarea spatiului liber

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Stream;
-
-
 public class Main {
     public static void main(String[] args) {
-
-
-
-
         int[] myArry = {2 , 4 , 6, 8 ,10};
         int b = 0 ;
-
         System.out.println("inreoduceti un numar de tip integer petru variabila b");
         try {
              b = new Scanner(System.in).nextInt();
@@ -21,7 +14,6 @@ public class Main {
             System.exit(1);
         };
         for(int i = 0 ; i< myArry.length;i++){
-
             if(myArry[i] < b){
                 System.out.println("Elementul din tablou " + myArry[i] + " < Elemnetul  b " + b);
             } else if (myArry[i] > b) {
@@ -30,13 +22,11 @@ public class Main {
                 System.out.println("Elementul din tablou " + myArry[i] + " = Elemnetul  b " + b);
             }
         }
-
         List<Integer> list = new ArrayList<>();
         list.add(b);
         list.add(5);
         list.add(7);
         list.add(8);
-
         System.out.println("Elementele din lista sunt :");
 
         for(int num : list) {
@@ -47,7 +37,4 @@ public class Main {
                 .filter(num -> num % 2 == 0)
                 .forEach(System.out::println);
     }
-
-
-
 }
